a5fa55302555b57ba99bb36e75647e6b45d470e5
MaximMilashchenko <mr.milashchenko@mail.ru>
2026-05-27 18:28:47 +0300

    reference converter fix

--- reference_solver/scripts/convert_solution_to_npy.py
+++ reference_solver/scripts/convert_solution_to_npy.py
@@ -6,7 +6,7 @@ xmax = 1000
 
 u_data = np.loadtxt("../data/raw/AH=5e+4/udata/u0000.dat")
 for i in range(1, tmax):
-    u_new_data = np.loadtxt("impdat/udata/u" + str(i).zfill(4) + ".dat")
+    u_new_data = np.loadtxt("../data/raw/AH=5e+4/udata/u" + str(i).zfill(4) + ".dat")
     u_data = np.dstack((u_data, u_new_data))
 
     if int(i/10) == i/10.:
@@ -15,7 +15,7 @@ for i in range(1, tmax):
 u_new_data = np.loadtxt("../data/raw/AH=5e+4/udata/u" + str(tmax).zfill(4) + ".dat")
 u_data = np.dstack((u_data, u_new_data))
 u_data = u_data[0].transpose()
-np.save("../data/pinn_ready/Zonal_Velocity", u_data)
+np.save("../data/pinn_ready/dt=1s_dx=400m/AH=5e+4/Zonal_Velocity", u_data)
 
 # Load and Plot Data for h
 h_data = np.loadtxt("../data/raw/AH=5e+4/zdata/z0000.dat")
@@ -29,4 +29,4 @@ for i in range(1, tmax):
 h_new_data = np.loadtxt("../data/raw/AH=5e+4/zdata/z" + str(tmax).zfill(4) + ".dat")
 h_data = np.dstack((h_data, h_new_data))
 h_data = h_data[0].transpose()
-np.save("../data/pinn_ready/Sea_Level_Elevation", h_data)
+np.save("../data/pinn_ready/dt=1s_dx=400m/AH=5e+4/Sea_Level_Elevation", h_data)
